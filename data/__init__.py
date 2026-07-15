804598d5fbeb2c2fc41e230e59cbdb990dde3ccd
LyaxZ <1554859306@qq.com>
2026-07-15 09:33:46 +0800

    refactor: 删除LSTM+Stacking+时序数据集，LightGBM已足够

--- data/__init__.py
+++ data/__init__.py
@@ -3,4 +3,3 @@ from data.loader import load_date_range, load_single_day, load_to_dataframe, get
 from data.label_builder import DisasterLabelBuilder
 from data.preprocessor import DataPreprocessor, quick_preprocess
 from data.splitter import TimeSeriesSplitter, split_by_season, split_by_date_range
-from data.dataset import WeatherSequenceDataset
